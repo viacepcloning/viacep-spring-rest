bedd58220b12160404127bbcf2666189dcd11b84
Marco Mangan <marco.mangan@gmail.com>
2021-11-07 00:40:01 -0300

    Heroku config

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+# viacep-spring-rest
---
+++ bin/src/main/resources/initDB.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS address (
+  id SERIAL,
+  cep VARCHAR(30),
+  logradouro VARCHAR(30),
+  complemento VARCHAR(30),
+  bairro VARCHAR(30),
+  localidade VARCHAR(30),
+  uf VARCHAR(30),
+  ibge VARCHAR(30),
+  gia VARCHAR(30),
+  ddd VARCHAR(30),
+  siafi VARCHAR(30),
+  CONSTRAINT pk_address PRIMARY KEY (id)
+);
+
+ALTER SEQUENCE address_id_seq RESTART WITH 100;
+
+
