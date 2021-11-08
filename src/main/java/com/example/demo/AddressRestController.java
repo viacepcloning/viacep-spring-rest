cad3403064706b24ae95c58259832ee5c22d6e0b
Marco Mangan <marco.mangan@gmail.com>
2021-11-08 15:38:04 -0300

    CORS

--- src/main/java/com/example/demo/AddressRestController.java
+++ src/main/java/com/example/demo/AddressRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@CrossOrigin(exposedHeaders = "errors, content-type")
 @RequestMapping("/api/addresses")
 public class AddressRestController {
 
