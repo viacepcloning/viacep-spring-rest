2aaf8cfe77e66294e516f5d151905cbdb5332dcc
Marco <marco.mangan@gmail.com>
2025-09-12 18:27:10 +0000

    Variante com H2

--- src/main/java/com/example/demo/ViacepApplication.java
+++ src/main/java/com/example/demo/ViacepApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * ViacepApplication is a simple postal address REST webservice.
@@ -16,11 +18,11 @@ public class ViacepApplication {
 		SpringApplication.run(ViacepApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner startup(AddressRepository addressRepository) {
-//		return args -> {
-//			addressRepository
-//					.save(new Address("90110170", "Rua Dezessete de Junho", "", "Menino Deus", "Porto Alegre", "RS"));
-//		};
-//	}
+	@Bean
+	public CommandLineRunner startup(AddressRepository addressRepository) {
+		return args -> {
+			addressRepository
+					.save(new Address("90110170", "Rua Dezessete de Junho", "", "Menino Deus", "Porto Alegre", "RS"));
+		};
+	}
 }
--- src/main/resources/populateDB.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO address(cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi) 
-            VALUES ('90110170', 'Rua Dezessete de Junho', 'complemento', 'Menino Deus', 'Porto Alegre', 'RS', 'ibge', 'gia', 'ddd', 'siafi') ON CONFLICT DO NOTHING;
