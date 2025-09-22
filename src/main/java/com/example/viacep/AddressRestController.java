9415df23c8741a48b12737531c1c5952e756d1a8
Marco <marco.mangan@gmail.com>
2025-09-22 01:21:32 +0000

    Removing variable from log.

--- src/main/java/com/example/viacep/AddressRestController.java
+++ src/main/java/com/example/viacep/AddressRestController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * {@code AddressRestController} retrieves a postal address by zip code.
  * 
- * Zip is a 8-digit string. 
+ * Zip is a 8-digit string.
  * 
  * @author marco.mangan@gmail.com
  *
@@ -29,35 +29,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddressRestController {
 
 	private Logger logger = Logger.getLogger(AddressRestController.class.getCanonicalName());
-	
+
 	private AddressRepository addressRepository;
 
 	@Autowired
 	public AddressRestController(AddressRepository addressRepository) {
-			this.addressRepository = addressRepository;
+		this.addressRepository = addressRepository;
 	}
 
 	/**
 	 * Gets a postal address by zip code.
 	 * 
 	 * @param zip an 8-digit string
-	 * @return an postal address, 
-	 * 			as HTTP status,
-	 * 			if zip was found, 200 OK, 
-	 * 			if zip was not found, 404 NOT FOUND,
-	 * 			if bad request, 500.
+	 * @return an postal address,
+	 *         as HTTP status,
+	 *         if zip was found, 200 OK,
+	 *         if zip was not found, 404 NOT FOUND,
+	 *         if bad request, 500.
 	 */
 	@GetMapping(value = "/{zip}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Address> getAddress(final @PathVariable String zip) {
-		String safeZip = zip;
-		if (safeZip != null) {
-			safeZip = safeZip.replaceAll("[\n\r]", "_");
-		}
-		logger.log(Level.INFO, "getAddress on: {0}", safeZip);
-		final Address address = addressRepository.findByCep(safeZip);
+
+		final Address address = addressRepository.findByCep(zip);
 		if (address == null) {
+			logger.log(Level.INFO, "getAddress not found.");
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
+		logger.log(Level.INFO, "getAddress ok.");
 		return new ResponseEntity<>(address, HttpStatus.OK);
 	}
 }
